0db0dc18e97c1522debf5cb9af86ee7dd07b468a
anastasiya-bkv <51928452+anastasiya-bkv@users.noreply.github.com>
2020-09-03 11:05:35 +0500

    Add files via upload

---
+++ precache-manifest.74bb14ca4ff4f9d944cc6ab1e8a8c76d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c03adc1854c69a22f80ded68004f94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff22f441f0fd58fa4dd6",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "151b84e7f419d10d3306",
+    "url": "/static/css/main.f08d949c.chunk.css"
+  },
+  {
+    "revision": "ff22f441f0fd58fa4dd6",
+    "url": "/static/js/2.07b0c27a.chunk.js"
+  },
+  {
+    "revision": "a4ef04eaee1dba4411b4440c0224ecda",
+    "url": "/static/js/2.07b0c27a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151b84e7f419d10d3306",
+    "url": "/static/js/main.95d7a307.chunk.js"
+  },
+  {
+    "revision": "1e71207ead225558b26f40ae0225fba5",
+    "url": "/static/js/main.95d7a307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf77bfcb77e97c8cd0b",
+    "url": "/static/js/runtime-main.cbdf1f68.js"
+  },
+  {
+    "revision": "d48003a0dfa1e753e0f694c5519c92c5",
+    "url": "/static/media/delete.d48003a0.svg"
+  },
+  {
+    "revision": "502b01008c4510dd673ea1ae3d3ac7fb",
+    "url": "/static/media/edit.502b0100.svg"
+  }
+]);
